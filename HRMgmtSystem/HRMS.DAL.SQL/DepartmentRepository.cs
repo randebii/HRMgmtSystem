2105b4cf62ba395e9142d41325cab0bec2a292c9
randebii <randebii@gmail.com>
2015-02-10 21:30:28 +0800

    Added some concrete repos

---
+++ HRMgmtSystem/HRMS.DAL.SQL/DepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using HRMS.Core.Contracts;
+using HRMS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.DAL.SQL
+{
+    public class DepartmentRepository : SingleIdRepository<Department, int>, IDepartmentRepository
+    {
+        public DepartmentRepository() : base("Departments") { }
+    }
+}
