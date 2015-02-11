e15c64956da958980fe2691c0b5296984c986b9d
randebii <randebii@gmail.com>
2015-02-11 18:09:47 +0800

    Added dapper to the unit test project

---
+++ HRMgmtSystem/HRMS.UnitTests/SQLRepository/GenericRepoTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HRMS.UnitTests.SQLRepository
+{
+    [TestClass]
+    public class GenericRepoTest
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+
+        }
+    }
+}
